b6436c5d34a109d0568180d12ecf1cc22adb6ee6
bangnokia <bangnokia@gmail.com>
2021-04-06 15:31:28 +0700

    add support for width and height

--- src/Components/Img.php
+++ src/Components/Img.php
@@ -2,32 +2,63 @@
 
 namespace Bangnokia\CdnImage\Components;
 
+use Illuminate\Support\Str;
 use Illuminate\View\Component;
 
 class Img extends Component
 {
     public string $src;
 
-    public string $cdnUrl;
+    public ?string $width;
 
-    public function __construct(string $src)
+    public ?string $height;
+
+    protected string $cdnUrl;
+
+    public function __construct(string $src, string $width = null, string $height = null)
     {
         $this->src = $src;
+        $this->width = $width;
+        $this->height = $height;
     }
 
     protected function makeCdnUrl()
     {
         ['host' => $host, 'path' => $path] = parse_url($this->src);
 
-        $this->cdnUrl = "https://cdn.statically.io/img/{$host}{$path}";
+        $this->cdnUrl = "https://cdn.statically.io/img/{$host}";
+        $filters = $this->makeFilter();
+
+        if ($filters->isNotEmpty()) {
+            $this->cdnUrl .= '/'.collect($filters)->map(fn($value, $key) => $key.'='.$value)->join(',');
+        }
+
+        $this->cdnUrl .= Str::start($path, '/');
 
         return $this;
+    }
+
+    protected function makeFilter()
+    {
+        $filters = [];
+
+        if ($this->width) {
+            $filters['w'] = $this->width;
+        }
+
+        if ($this->height) {
+            $filters['h'] = $this->height;
+        }
+
+        return collect($filters)->filter();
     }
 
     public function render()
     {
         $this->makeCdnUrl();
 
-        return view('cdn-image::img');
+        return view('cdn-image::img', [
+            'cdnUrl' => $this->cdnUrl
+        ]);
     }
 }
--- tests/ImgComponentTest.php
+++ tests/ImgComponentTest.php
@@ -20,6 +20,17 @@ class ImgComponentTest extends TestCase
         $view->assertSee('<img src="https://cdn.statically.io/img/github.com/lmao.jpg" >', false);
     }
 
+    public function test_it_can_accept_width_and_height()
+    {
+        $view = $this->blade('<x-img src="http://github.com/lmao.jpg" width="200" height="100" />');
+
+        $view->assertSee('<img src="https://cdn.statically.io/img/github.com/w=200,h=100/lmao.jpg" >', false);
+
+        $view = $this->blade('<x-img src="http://github.com/lmao.jpg" width="100" />');
+
+        $view->assertSee('<img src="https://cdn.statically.io/img/github.com/w=100/lmao.jpg" >', false);
+    }
+
     public function test_it_can_accept_attributes()
     {
         $view = $this->blade('<x-img src="http://github.com/lmao.jpg" name="foo" alt="bar"/>');
